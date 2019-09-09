6b4aad32f6acfd6a9b33f78cc2cbdaf518f1e535
sage-jordan <sagemjrodan@gmail.com>
2019-09-09 19:14:19 -0400

    Added styling to userCard

--- my-app/src/userCard.js
+++ my-app/src/userCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 
 class UserCard extends Component {
     constructor(props){
@@ -9,10 +10,17 @@ class UserCard extends Component {
     }
     render() {
         console.log("2nd Render:", this.props.userData);
+        const user = this.props.userData;
         return (
             <div className="userCard">
                 <h1>UserCard</h1>
-                <h2>{`Login: ${this.props.userData.login}`}</h2>
+                <Card
+                    image={user.avatar_url}
+                    header={user.name}
+                    meta={user.type}
+                    description={user.bio}
+                    extra={user.location}
+                />
             </div>
         )
     }   
